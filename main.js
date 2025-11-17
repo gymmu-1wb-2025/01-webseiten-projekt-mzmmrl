6d4c737f2901a17fac9b56b57db93d84c7a65d8f
mzmmrl <e273626@edu.sbl.ch>
2025-11-17 09:15:57 +0000

    .

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Hello world!!!");
+let counter = 0;
+
+function createNewElement() {
+  let newElement = document.createElement("p");
+
+  counter = counter + 1;
+
+  newElement.textcontent = "Element " + counter;
+
+  let mainElement = document.querySelector("main");
+  mainElement.appendChild(newElement);
+}
